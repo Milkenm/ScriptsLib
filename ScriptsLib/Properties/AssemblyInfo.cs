8378142540527744c6cb86b215762b032aa56e92
Milkenm <miguel.campos.johnny@gmail.com>
2019-04-29 16:21:48 +0100

    Added the copyright stuff. Minor changes.

--- ScriptsLib/Properties/AssemblyInfo.cs
+++ ScriptsLib/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("ScriptsLib")]
-[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyCopyright("Copyright © Milkenm 2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
