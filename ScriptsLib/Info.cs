e0cecaf9f1e5fdef1b389ceb22bf59b240cb4f3d
Milkenm <miguel.campos.johnny@gmail.com>
2019-04-30 21:12:45 +0100

    Changed Version.

--- ScriptsLib/Info.cs
+++ ScriptsLib/Info.cs
@@ -4,7 +4,7 @@
 	{
 		public static readonly string Author = "Milkenm";
 		public static readonly string Contributors = null;
-		public static readonly string Version = "1.0.1.0";
-		public static readonly string ReleaseDate = "28/04/2019 - 17:32";
+		public static readonly string Version = "1.0.0.0";
+		public static readonly string ReleaseDate = "30/04/2019 - 21:12";
 	}
 }
