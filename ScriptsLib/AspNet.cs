a2f0d9935a348041aada51ce8d67aee3f8b62d40
Milkenm <miguel.campos.johnny@gmail.com>
2019-06-14 16:22:26 +0100

    I'm dumb

--- ScriptsLib/AspNet.cs
+++ ScriptsLib/AspNet.cs
@@ -9,12 +9,16 @@ namespace ScriptsLib.AspNet
 	public class AspNet
 	{
 		#region Show Pop-up
+		///
+		// https://stackoverflow.com/questions/16287515/how-to-get-a-server-side-asp-page-to-display-a-pop-up-message-in-the-clients-br
+		///
+
 		/// <summary>Shows a pop-up message on an ASP.NET application.</summary>
 		/// <param name="_Page">The page used to send the pop-up.</param>
 		/// <param name="_Message">The message to be displayed in the pop-up.</param>
 		public void ShowPopup(Page _Page, string _Message)
 		{
-			_Page.Response.Write("<script language=javascript>alert('Message here.')</script>");
+			_Page.Response.Write($"<script language=javascript>alert('{_Message}')</script>");
 		}
 		#endregion Show Pop-up
 	}
