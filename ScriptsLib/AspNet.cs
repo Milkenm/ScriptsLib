cd9253147830a278d963e00bb801afde874e2a27
Milkenm <miguel.campos.johnny@gmail.com>
2019-06-14 16:17:25 +0100

    AspNet Pop-up

---
+++ ScriptsLib/AspNet.cs
@@ -0,0 +1,21 @@
+﻿#region Usings
+using System.Web.UI;
+#endregion Usings
+
+
+
+namespace ScriptsLib.AspNet
+{
+	public class AspNet
+	{
+		#region Show Pop-up
+		/// <summary>Shows a pop-up message on an ASP.NET application.</summary>
+		/// <param name="_Page">The page used to send the pop-up.</param>
+		/// <param name="_Message">The message to be displayed in the pop-up.</param>
+		public void ShowPopup(Page _Page, string _Message)
+		{
+			_Page.Response.Write("<script language=javascript>alert('Message here.')</script>");
+		}
+		#endregion Show Pop-up
+	}
+}
