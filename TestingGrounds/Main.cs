36f7a790adcc43c01084185e60561b7ff6d1f824
Milkenm <miguel.campos.johnny@gmail.com>
2019-08-21 13:13:19 +0100

    Test button stuff.

--- TestingGrounds/Main.cs
+++ TestingGrounds/Main.cs
@@ -4,11 +4,14 @@ using System.Collections.Generic;
 using System.Drawing;
 using System.IO;
 using System.Net;
+using System.Net.NetworkInformation;
 using System.Numerics;
 using System.Text;
-using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+
+using Newtonsoft.Json;
+
 using ScriptsLib;
 using ScriptsLib.Controls;
 using ScriptsLib.Databases;
@@ -116,8 +119,111 @@ namespace TestingGrounds
 		// # ================================================================================================ #
 		private void button_tg_test_Click(object sender, EventArgs e)
 		{
-			
+			int _TestIndex = 3;
+			try
+			{
+				if (_TestIndex == 1)
+				{
+					Testing1();
+				}
+				else if (_TestIndex == 2)
+				{
+					Testing2();
+				}
+				else if (_TestIndex == 3)
+				{
+					Testing3();
+				}
+			}
+			catch (Exception _Exception)
+			{
+				Ex(_Exception);
+			}
 		}
+
+		#region Testing 1 - League of Legends API
+		// # ================================================================================================ #
+		void Testing1()
+		{
+			string _ApiKey = "";
+
+			MessageBox.Show(GET("https://euw1.api.riotgames.com/lol/platform/v3/champion-rotations?api_key=" + _ApiKey));
+			string json = "jj";
+			JsonConvert.DeserializeObject<RootChampionDTO>(json);
+		}
+
+		public class RootChampionDTO
+		{
+			public string Type { get; set; }
+			public string Version { get; set; }
+			public Dictionary<string, Champion> Data { get; set; }
+		}
+
+		public class Champion
+		{
+			public int id { get; set; }
+			public string key { get; set; }
+			public string name { get; set; }
+			public string title { get; set; }
+		}
+
+		public string GET(string url)
+		{
+			HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+
+			try
+			{
+				WebResponse response = request.GetResponse();
+				using (Stream responseStream = response.GetResponseStream())
+				{
+					StreamReader reader = new StreamReader(responseStream, Encoding.UTF8);
+					return reader.ReadToEnd();
+				}
+			}
+			catch (WebException ex)
+			{
+				WebResponse errorResponse = ex.Response;
+				using (Stream responseStream = errorResponse.GetResponseStream())
+				{
+					StreamReader reader = new StreamReader(responseStream, Encoding.GetEncoding("utf-8"));
+					String errorText = reader.ReadToEnd();
+				}
+				throw;
+			}
+		}
+		// # ================================================================================================ #
+		#endregion Testing 1 - League of Legends API
+
+		#region Testing 2 - Packets
+		// # ================================================================================================ #
+		void Testing2()
+		{
+			IPGlobalProperties a = IPGlobalProperties.GetIPGlobalProperties();
+			TcpConnectionInformation[] con = a.GetActiveTcpConnections();
+			foreach (TcpConnectionInformation tcp in con)
+			{
+				MessageBox.Show(tcp.LocalEndPoint.Address.ToString());
+			}
+		}
+		// # ================================================================================================ #
+		#endregion Testing 2 - Packets
+
+		#region Testing 3 - Packets 2
+		// # ================================================================================================ #
+		void Testing3()
+		{
+			HttpListener l = new HttpListener();
+			AsyncCallback callback = new AsyncCallback(Callback);
+			l.Start();
+			l.BeginGetContext(callback, "h");
+		}
+
+		static void Callback(IAsyncResult result)
+		{
+			MessageBox.Show(result.AsyncState.ToString());
+		}
+		// # ================================================================================================ #
+		#endregion Testing 3 - Packets 2
 		// # ================================================================================================ #
 		#endregion Test Button
 
