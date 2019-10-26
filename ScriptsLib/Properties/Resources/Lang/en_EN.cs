5518fb298fad01aaa6bdb20a653db5c63858c31a
Milkenm <miguel.campos.johnny@gmail.com>
2019-10-26 02:37:22 +0100

    Changes to the language files (that I don't even know where I use them)

--- ScriptsLib/Properties/Resources/Lang/en_EN.cs
+++ ScriptsLib/Properties/Resources/Lang/en_EN.cs
@@ -1,12 +1,12 @@
 ﻿namespace ScriptsLib.Resources.Lang
 {
-	internal class en_EN
+	internal static class en_EN
 	{
 		#region Error Messages
-		internal readonly string _Language = "English (en_EN)";
-		internal readonly string _ScriptsLibTitle = "ScriptsLib";
-		internal readonly string _ErrorTitle = $"{new en_EN()._ScriptsLibTitle} - Error";
-		internal readonly string _ErrorSocketException = "There was an error creating a connection to the remote server.Check if the remote IP adress and port number are correct or that the remote server is not offline.\n\n\n\n\nDetails:\n\n{0}";
+		internal static readonly string Language = "English (en_EN)";
+		internal static readonly string ScriptsLibTitle = "ScriptsLib";
+		internal static readonly string ErrorTitle = $"{ScriptsLibTitle} - Error";
+		internal static readonly string ErrorSocketException = "There was an error creating a connection to the remote server. Check if the remote IP adress and port number are correct or that the remote server is not offline.\n\n\n\n\nDetails:\n\n{0}";
 		#endregion Error Messages
 	}
 }
