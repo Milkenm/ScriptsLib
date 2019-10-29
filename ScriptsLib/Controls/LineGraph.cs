7236db4930fcf8a64a3d149b8768373da39ec573
Milkenm <miguel.campos.johnny@gmail.com>
2019-10-29 21:37:11 +0000

    Add LineGraph Control

---
+++ ScriptsLib/Controls/LineGraph.cs
@@ -0,0 +1,130 @@
+﻿#region Usings
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.Design;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Windows.Forms.Design;
+#endregion Usings
+
+
+
+namespace Testing.LineGraph
+{
+	public partial class LineGraph : UserControl
+	{
+		private  List<Line> lines = new List<Line>();
+		private Graphics g;
+		private int CurrentX, CurrentY;
+
+		public LineGraph()
+		{
+			InitializeComponent();
+
+			g = this.CreateGraphics();
+
+
+		}
+
+		private void LineGraph_Paint(object sender, PaintEventArgs e)
+		{
+			g.Clear(Color.White);
+
+			foreach (Line l in lines)
+			{
+				DrawLine(l, false);
+			}
+		}
+
+		public struct Line
+		{
+			public Pen P;
+			public int StartX, StartY, EndX, EndY;
+			public PositionList Position;
+
+			public Line(Pen _P, int _StartX, int _StartY, int _EndX, int _EndY, PositionList _Pos)
+			{
+				P = _P;
+				StartX = _StartX;
+				StartY = _StartY;
+				EndX = _EndX;
+				EndY = _EndY;
+				Position = _Pos;
+			}
+		}
+
+		public enum PositionList
+		{
+			BottomLeft,
+			BottomRight,
+			TopLeft,
+			TopRight,
+		}
+		
+		public void DrawLine(Line line) => DrawLine(line.P, line.StartX, line.StartY, line.EndX, line.EndY, line.Position, true);
+		public void DrawLine(Line line, bool SaveLine) => DrawLine(line.P, line.StartX, line.StartY, line.EndX, line.EndY, line.Position, SaveLine);
+		public void DrawLine(Pen pen, int StartX, int StartY, int EndX, int EndY, PositionList Pos, bool SaveLine)
+		{
+			switch (Pos)
+			{
+				case PositionList.BottomLeft:
+					{
+						g.DrawLine(pen, StartX, this.Size.Height - StartY, EndX, this.Size.Height - EndY);
+						break;
+					}
+				case PositionList.BottomRight:
+					{
+						g.DrawLine(pen, this.Size.Width - StartX, this.Size.Height - StartY, this.Size.Width - EndX, this.Size.Height - EndY);
+						break;
+					}
+				case PositionList.TopLeft:
+					{
+						g.DrawLine(pen, StartX, StartY, EndX, EndY);
+						break;
+					}
+				case PositionList.TopRight:
+					{
+						g.DrawLine(pen, this.Size.Width - StartX, StartY, this.Size.Width - EndX, EndY);
+						break;
+					}
+			}
+
+			if (SaveLine) lines.Add(new Line(pen, StartX, StartY, EndX, EndY, Pos));
+
+			CurrentX = EndX;
+			CurrentY = EndY;
+		}
+
+		public void IncrementLine(Pen pen, int EndX, int EndY, PositionList Pos, bool SaveLine)
+		{
+			switch (Pos)
+			{
+				case PositionList.BottomLeft:
+					{
+						g.DrawLine(pen, CurrentX, this.Size.Height - CurrentY, EndX, this.Size.Height - EndY);
+						break;
+					}
+				case PositionList.BottomRight:
+					{
+						g.DrawLine(pen, this.Size.Width - CurrentX, this.Size.Height - CurrentY, this.Size.Width - EndX, this.Size.Height - EndY);
+						break;
+					}
+				case PositionList.TopLeft:
+					{
+						g.DrawLine(pen, CurrentX, CurrentY, EndX, EndY);
+						break;
+					}
+				case PositionList.TopRight:
+					{
+						g.DrawLine(pen, this.Size.Width - CurrentX, CurrentY, this.Size.Width - EndX, EndY);
+						break;
+					}
+			}
+
+			if (SaveLine) lines.Add(new Line(pen, CurrentX, CurrentY, EndX, EndY, Pos));
+
+			CurrentX = EndX;
+			CurrentY = EndY;
+		}
+	}
+}
---
+++ ScriptsLib/Controls/LineGraph.designer.cs
@@ -0,0 +1,47 @@
+﻿namespace Testing.LineGraph
+{
+	partial class LineGraph
+	{
+		/// <summary> 
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary> 
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Component Designer generated code
+
+		/// <summary> 
+		/// Required method for Designer support - do not modify 
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.SuspendLayout();
+			// 
+			// LineGraph
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+			this.Name = "LineGraph";
+			this.Size = new System.Drawing.Size(198, 98);
+			this.Paint += new System.Windows.Forms.PaintEventHandler(this.LineGraph_Paint);
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+	}
+}
