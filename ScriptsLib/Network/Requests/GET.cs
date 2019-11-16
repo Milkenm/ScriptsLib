4a3969f17e4a203af9511f87ece9bef9aec96adf
Milkenm <miguel.campos.johnny@gmail.com>
2019-11-16 13:02:36 +0000

    Add Network.Requests.GET

---
+++ ScriptsLib/Network/Requests/GET.cs
@@ -0,0 +1,27 @@
+﻿#region Usings
+using System.IO;
+using System.Net;
+using System.Text;
+#endregion Usings
+
+
+
+namespace ScriptsLib.Network.Requests
+{
+	public static partial class Requests
+	{
+		/// <summary>Executes a GET Resquest.</summary>
+		/// <param name="url">The request URL (or API URL).</param>
+		public static string GET(string url)
+		{
+			HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+
+			WebResponse response = request.GetResponse();
+			using (Stream stream = response.GetResponseStream())
+			{
+				StreamReader reader = new StreamReader(stream, Encoding.UTF8);
+				return reader.ReadToEnd();
+			}
+		}
+	}
+}
