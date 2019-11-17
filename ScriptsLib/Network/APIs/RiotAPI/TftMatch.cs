b4b071b3ac19c2d5ba6fdc937c5746c9f7d38fd7
Milkenm <miguel.campos.johnny@gmail.com>
2019-11-17 20:11:38 +0000

    RiotAPI (TftMatch)

---
+++ ScriptsLib/Network/APIs/RiotAPI/TftMatch.cs
@@ -0,0 +1,20 @@
+﻿#region Usings
+using static ScriptsLib.Network.Requests;
+#endregion Usings
+
+
+
+namespace ScriptsLib.Network.APIs
+{
+	public static partial class RiotAPI
+	{
+		public static class TftMatch // v1
+		{
+			/// <summary>Get a list of match ids by PUUID.</summary>
+			public static string GetMatchIds(string encryptedPUUID) => GET(ServerString() + $"/tft/match/v1/matches/by-puuid/{encryptedPUUID}/ids" + ApiString());
+
+			/// <summary>Get a match by match id.</summary>
+			public static string GetMatchById(string matchId) => GET(ServerString() + $"/tft/match/v1/matches/{matchId}" + ApiString());
+		}
+	}
+}
