7b8c24eafc3cc7575d574884b48511f0efdccb44
Milkenm <miguel.campos.johnny@gmail.com>
2019-11-17 20:10:18 +0000

    RiotAPI (Spectator)

---
+++ ScriptsLib/Network/APIs/RiotAPI/Spectator.cs
@@ -0,0 +1,21 @@
+﻿#region Usings
+using static ScriptsLib.Network.Requests;
+#endregion Usings
+
+
+
+namespace ScriptsLib.Network.APIs
+{
+	public static partial class RiotAPI
+	{
+		public static class Spectator // v4
+		{
+			/// <summary>Get current game information for the given summoner ID</summary>
+			/// <param name="encryptedSummonerId">The ID of the summoner.</param>
+			public static string GetCurrentGameInfo(string encryptedSummonerId) => GET(ServerString() + $"/lol/spectator/v4/active-games/by-summoner/{encryptedSummonerId}" + ApiString());
+
+			/// <summary>Get list of featured games.</summary>
+			public static string GetFeaturedGames() => GET(ServerString() + "/lol/spectator/v4/featured-games" + ApiString());
+		}
+	}
+}
