9764b919337115fcf0c4b729f739e385870aafcd
Milkenm <miguel.campos.johnny@gmail.com>
2019-11-17 20:08:14 +0000

    RiotAPI (Champion)

---
+++ ScriptsLib/Network/APIs/RiotAPI/Champion.cs
@@ -0,0 +1,17 @@
+﻿#region Usings
+using static ScriptsLib.Network.Requests;
+#endregion Usings
+
+
+
+namespace ScriptsLib.Network.APIs
+{
+	public static partial class RiotAPI
+	{
+		public static class Champion // v3
+		{
+			/// <summary>Returns champion rotations, including free-to-play and low-level free-to-play rotations (REST).</summary>
+			public static string GetChampionRotation() => GET(ServerString() + "/lol/platform/v3/champion-rotations" + ApiString());
+		}
+	}
+}
