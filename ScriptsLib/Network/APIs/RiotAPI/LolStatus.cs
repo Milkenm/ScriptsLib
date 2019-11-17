87df22000be5d4e468a3fd0a5b1a8e1610d688ab
Milkenm <miguel.campos.johnny@gmail.com>
2019-11-17 20:09:06 +0000

    RiotAPI (LolStatus)

---
+++ ScriptsLib/Network/APIs/RiotAPI/LolStatus.cs
@@ -0,0 +1,17 @@
+﻿#region Usings
+using static ScriptsLib.Network.Requests;
+#endregion Usings
+
+
+
+namespace ScriptsLib.Network.APIs
+{
+	public static partial class RiotAPI
+	{
+		public static class LolStatus // v3
+		{
+			/// <summary>Get League of Legends status for the given shard.</summary>
+			public static string GetLolStatus() => GET(ServerString() + "/lol/status/v3/shard-data" + ApiString());
+		}
+	}
+}
