bc1e93a35f36a71d2ec72fcc188dd35c21d8aeb6
Milkenm <miguel.campos.johnny@gmail.com>
2019-11-17 20:48:41 +0000

    RiotAPI (ThirdPartyCode)

---
+++ ScriptsLib/Network/APIs/RiotAPI/ThirdPartyCode.cs
@@ -0,0 +1,17 @@
+﻿#region Usings
+using static ScriptsLib.Network.Requests;
+#endregion Usings
+
+
+
+namespace ScriptsLib.Network.APIs
+{
+	public static partial class RiotAPI
+	{
+		public static class ThirdPartyCode // v4
+		{
+			/// <summary>Get third party code for a given summoner ID.</summary>
+			public static string GetThirdPartyCodeBySummonerId(string encryptedSummonerId) => GET(ServerString() + $"/lol/platform/v4/third-party-code/by-summoner/{encryptedSummonerId}" + ApiString());
+		}
+	}
+}
