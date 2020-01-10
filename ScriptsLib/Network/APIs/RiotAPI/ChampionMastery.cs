a45f7841fbddb36a17674d9ece85a8f8de3ec4a7
Milkenm <miguel.campos.johnny@gmail.com>
2020-01-10 17:51:48 +0000

    Riot API: ChampionMastery JSON

--- ScriptsLib/Network/APIs/RiotAPI/ChampionMastery.cs
+++ ScriptsLib/Network/APIs/RiotAPI/ChampionMastery.cs
@@ -10,6 +10,35 @@ namespace ScriptsLib.Network.APIs
 	{
 		public static class ChampionMastery // v4
 		{
+			#region JSON
+			/// <summary>This object contains single Champion Mastery information for player and champion combination.</summary>
+			public class ChampionMasteryDTO
+			{
+				/// <summary>Is chest granted for this champion or not in current season.</summary>
+				public bool chestGranted;
+				/// <summary>Champion level for specified player and champion combination.</summary>
+				public int championLevel;
+				/// <summary>Total number of champion points for this player and champion combination - they are used to determine championLevel.</summary>
+				public int championPoints;
+				/// <summary>Champion ID for this entry.</summary>
+				public long championId;
+				/// <summary>Number of points needed to achieve next level. Zero if player reached maximum champion level for this champion.</summary>
+				public long championPointsUntilNextLevel;
+				/// <summary>Last time this champion was played by this player - in Unix milliseconds time format.</summary>
+				public long lastPlayTime;
+				/// <summary>The token earned for this champion to levelup.</summary>
+				public int tokensEarned;
+				/// <summary>Number of points earned since current level has been achieved.</summary>
+				public long championPointsSinceLastLevel;
+				/// <summary>Summoner ID for this entry. (Encrypted)</summary>
+				public string summonerId;
+			}
+			#endregion JSON
+
+
+
+
+
 			/// <summary>Get all champion mastery entries sorted by number of champion points descending.</summary>
 			/// <param name="encryptedSummonerId">Summoner ID associated with the player.</param>
 			public static string GetChampionMasteryList(string encryptedSummonerId)
