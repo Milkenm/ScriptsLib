ce8138503f088e370486d600f8e51ce821db8a22
Milkenm <miguel.campos.johnny@gmail.com>
2020-01-10 17:51:41 +0000

    Riot API: Champion JSON

--- ScriptsLib/Network/APIs/RiotAPI/Champion.cs
+++ ScriptsLib/Network/APIs/RiotAPI/Champion.cs
@@ -1,4 +1,6 @@
 ﻿#region Usings
+using System.Collections.Generic;
+
 using static ScriptsLib.Network.Requests;
 #endregion Usings
 
@@ -10,6 +12,19 @@ namespace ScriptsLib.Network.APIs
 	{
 		public static class Champion // v3
 		{
+			#region JSON
+			public class ChampionInfo
+			{
+				public List<int> freeChampionIdsForNewPlayers { get; set; }
+				public List<int> freeChampionIds { get; set; }
+				public int maxNewPlayerLevel { get; set; }
+			}
+			#endregion JSON
+
+
+
+
+
 			/// <summary>Returns champion rotations, including free-to-play and low-level free-to-play rotations (REST).</summary>
 			public static string GetChampionRotation()
 			{
