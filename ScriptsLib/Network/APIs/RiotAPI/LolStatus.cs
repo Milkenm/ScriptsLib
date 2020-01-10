82c3ca8c45895b695b1b944a1e8c7081e32797a1
Milkenm <miguel.campos.johnny@gmail.com>
2020-01-10 17:52:04 +0000

    Riot API: LolStatus JSON

--- ScriptsLib/Network/APIs/RiotAPI/LolStatus.cs
+++ ScriptsLib/Network/APIs/RiotAPI/LolStatus.cs
@@ -1,4 +1,6 @@
 ﻿#region Usings
+using System.Collections.Generic;
+
 using static ScriptsLib.Network.Requests;
 #endregion Usings
 
@@ -10,6 +12,56 @@ namespace ScriptsLib.Network.APIs
 	{
 		public static class LolStatus // v3
 		{
+			#region JSON
+			public class ShardStatus
+			{
+				public string name;
+				public string region_tag;
+				public string hostname;
+				public List<Service> services;
+				public string slug;
+				public List<string> locales;
+			}
+
+			public class Service
+			{
+				public string status;
+				public List<Incident> incidents;
+				public string name;
+				public string slug;
+			}
+
+			public class Incident
+			{
+				public bool active;
+				public string created_at;
+				public long id;
+				public List<Message> updates;
+			}
+
+			public class Message
+			{
+				public string severity;
+				public string author;
+				public string created_at;
+				public List<Translation> translations;
+				public string updated_at;
+				public string content;
+				public string id;
+			}
+
+			public class Translation
+			{
+				public string locale;
+				public string content;
+				public string updated_at;
+			}
+			#endregion JSON
+
+
+
+
+
 			/// <summary>Get League of Legends status for the given shard.</summary>
 			public static string GetLolStatus()
 			{
