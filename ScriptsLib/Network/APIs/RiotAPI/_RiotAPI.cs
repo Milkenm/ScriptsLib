dc15dffb0d5b9b174c7559fad744fa435c0dfe3b
Milkenm <miguel.campos.johnny@gmail.com>
2020-01-14 09:38:04 +0000

    Add Response Codes

--- ScriptsLib/Network/APIs/RiotAPI/_RiotAPI.cs
+++ ScriptsLib/Network/APIs/RiotAPI/_RiotAPI.cs
@@ -60,6 +60,21 @@
 			III,
 			IV,
 		}
+
+		public enum ResponseCodes
+		{
+			BadRequest = 400,
+			Unauthorized = 401,
+			Forbidden = 403,
+			DataNotFound = 404,
+			MethodNotAllowed = 405,
+			UnsupportedMediaType = 415,
+			RateLimitExceeded = 429,
+			InternalServerError = 500,
+			BadGateway = 502,
+			ServiceUnavailable = 503,
+			GatewayTimeout = 504,
+		}
 		#endregion Enums
 
 		public static string ApiKey;
