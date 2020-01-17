1ab926c23e379aabb78d9f589b05d95bb3db1449
Milkenm <miguel.campos.johnny@gmail.com>
2020-01-17 11:07:47 +0000

    AccessDatabase.ExecuteSQL

---
+++ ScriptsLib/Databases/Access/Execute SQL.cs
@@ -0,0 +1,27 @@
+﻿using System.Data.OleDb;
+
+using static ScriptsLib.Dev;
+
+
+
+namespace ScriptsLib.nDatabases
+{
+	public static partial class AccessDatabase
+	{
+		public static OleDbDataReader ExecuteSQL(string sql)
+		{
+			OleDbConnection con = new OleDbConnection(_BaseConnection + _DatabasePath);
+
+			OleDbCommand command;
+			command = new OleDbCommand(sql, con);
+
+			Msg(command.CommandText, MsgType.Info, "Access ExecuteSQL");
+			
+			con.Open();
+			OleDbDataReader dr = command.ExecuteReader();
+			con.Close();
+
+			return dr;
+		}
+	}
+}
