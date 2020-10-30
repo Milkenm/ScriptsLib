c0b5dba4a67ffcfcb7f38cbc2e8836c933a75ff6
Milkenm <miguel.campos.johnny@gmail.com>
2020-10-30 09:58:13 +0000

    Update Show Pop-up.cs

--- ScriptsLib/AspNet/Show Pop-up.cs
+++ ScriptsLib/AspNet/Show Pop-up.cs
@@ -7,7 +7,7 @@ namespace ScriptsLib
 		/// <summary>Shows a pop-up message on an ASP.NET application.</summary>
 		/// <param name="page">The page to send the pop-up to.</param>
 		/// <param name="message">The message to be displayed in the pop-up.</param>
-		// https://stackoverflow.com/a/16302541
+		/// https://stackoverflow.com/a/16302541
 		public static void ShowPopup(Page page, string message)
 		{
 			page.Response.Write($"<script language=javascript>alert('{message}')</script>");
