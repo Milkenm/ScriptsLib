d97cd95cddfbbe764cb121cc1d9fdad486ed8c5d
Milkenm <miguel.campos.johnny@gmail.com>
2020-10-30 10:22:21 +0000

    PInvoke (User32)

---
+++ ScriptsLib/PInvoke/User32.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace ScriptsLib.PInvoke
+{
+	public static class User32
+	{
+		[DllImport("user32.dll")]
+		internal static extern int SendMessage(IntPtr hWnd, int Msg, int wParam, int lParam);
+
+		[DllImport("user32.dll")]
+		internal static extern bool ReleaseCapture();
+	}
+}
