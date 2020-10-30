3be30325d5acfbdd50cf36b4a542b818e800d9ce
Milkenm <miguel.campos.johnny@gmail.com>
2020-10-30 11:05:46 +0000

    Update ShowConfirmationDialog.cs

--- ScriptsLib/Controls/Tweaks/MessageBox/ShowConfirmationDialog.cs
+++ ScriptsLib/Controls/Tweaks/MessageBox/ShowConfirmationDialog.cs
@@ -1,28 +1,24 @@
-﻿#region Usings
-using System.Windows.Forms;
-#endregion Usings
-
-
+﻿using System.Windows.Forms;
 
 namespace ScriptsLib.Controls.Tweaks
 {
 	public static partial class SlMessageBox
 	{
 		/// <summary>Shows a MessageBox with yes and no options.</summary>
-		/// <param name="_Title">The title of the confirmation dialog.</param>
-		/// <param name="_Message">The message to be displayed in the confirmation dialog.</param>
-		/// <param name="_Icon">Icon of the confirmation dialog.</param>
-		public static bool ShowConfirmationDialog(string _Title, string _Message, MessageBoxIcon? _Icon = null)
+		/// <param name="title">The title of the confirmation dialog.</param>
+		/// <param name="message">The message to be displayed in the confirmation dialog.</param>
+		/// <param name="icon">Icon of the confirmation dialog.</param>
+		public static bool ShowConfirmationDialog(string title, string message, MessageBoxIcon? icon = null)
 		{
 			DialogResult _Dialog;
 
-			if (_Icon != null)
+			if (icon != null)
 			{
-				_Dialog = MessageBox.Show(_Message, _Title, MessageBoxButtons.YesNo, (MessageBoxIcon)_Icon);
+				_Dialog = MessageBox.Show(message, title, MessageBoxButtons.YesNo, (MessageBoxIcon)icon);
 			}
 			else
 			{
-				_Dialog = MessageBox.Show(_Message, _Title, MessageBoxButtons.YesNo);
+				_Dialog = MessageBox.Show(message, title, MessageBoxButtons.YesNo);
 			}
 
 			if (_Dialog == DialogResult.Yes)
