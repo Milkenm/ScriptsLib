3aaa07c1fc3b2ccd02016126cb22d57e266e9797
Milkenm <miguel.campos.johnny@gmail.com>
2020-11-01 00:49:14 +0000

    Update CalculateCombinations.cs

--- ScriptsLib/Math/CalculateCombinations.cs
+++ ScriptsLib/Math/CalculateCombinations.cs
@@ -1,25 +1,21 @@
-﻿#region Usings
-using System.Numerics;
-#endregion Usings
-
-
+﻿using System.Numerics;
 
 namespace ScriptsLib
 {
 	public static partial class QuickMath
 	{
 		/// <summary>Returns the number of combinations possible with the given elements and group.</summary>
-		/// <param name="_Elements">How many elements to choose...</param>
-		/// <param name="_Group">...from a group of?</param>
-		public static BigInteger CalculateCombinations(BigInteger _Elements, BigInteger _Group)
+		/// <param name="elements">How many elements to choose...</param>
+		/// <param name="group">...from a group of?</param>
+		public static BigInteger CalculateCombinations(BigInteger elements, BigInteger group)
 		{
-			if (CalculateFactorial(_Group - _Elements) == 0)
+			if (CalculateFactorial(group - elements) == 0)
 			{
 				return 1;
 			}
 			else
 			{
-				return CalculateFactorial(_Group) / (CalculateFactorial(_Elements) * CalculateFactorial(_Group - _Elements));
+				return CalculateFactorial(group) / (CalculateFactorial(elements) * CalculateFactorial(group - elements));
 			}
 		}
 	}
