9aaec36314f09092d82b0a1fa55a72f96abc072e
Milkenm <miguel.campos.johnny@gmail.com>
2020-11-01 00:58:12 +0000

    Update ExecuteCmdCommand.cs

--- ScriptsLib/Tools/ExecuteCmdCommand.cs
+++ ScriptsLib/Tools/ExecuteCmdCommand.cs
@@ -1,36 +1,22 @@
-﻿#region Usings
-using System;
-using System.Diagnostics;
-
-using static ScriptsLib.Dev;
-#endregion Usings
-
-// # = #
-// https://stackoverflow.com/questions/1469764/run-command-prompt-commands
-// # = #
+﻿using System.Diagnostics;
 
 namespace ScriptsLib
 {
 	public static partial class Tools
 	{
 		/// <summary>Executes a command line command.</summary>
-		/// <param name="_Command">The command to run.</param>
-		public static void ExecuteCmdCommand(string _Command)
+		/// <param name="command">The command to run.</param>
+		/// https://stackoverflow.com/a/1469790
+		public static void ExecuteCmdCommand(string command)
 		{
-			try
+			Process p = new Process();
+			p.StartInfo = new ProcessStartInfo()
 			{
-				Process _Process = new Process();
-				ProcessStartInfo _StartInfo = new ProcessStartInfo();
-				_StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
-				_StartInfo.FileName = "cmd.exe";
-				_StartInfo.Arguments = $"/C {_Command}";
-				_Process.StartInfo = _StartInfo;
-				_Process.Start();
-			}
-			catch (Exception _Exception)
-			{
-				Msg(_Exception.Message, MsgType.Error, _Exception.Source);
-			}
+				WindowStyle = ProcessWindowStyle.Hidden,
+				FileName = "cmd.exe",
+				Arguments = $"/C {command}",
+			};
+			p.Start();
 		}
 	}
 }
