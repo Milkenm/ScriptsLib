f736ac55f035566bf461ee1341dfe13ef49fdbd2
Milkenm <miguel.campos.johnny@gmail.com>
2020-11-01 23:29:13 +0000

    Update Requests

---
+++ ScriptsLib/Network/Requests/Reqs/POST.cs
@@ -0,0 +1,43 @@
+﻿using System.IO;
+using System.Net;
+
+namespace ScriptsLib.Network
+{
+	public static partial class Requests
+	{
+		/// <summary>Executes a POST Resquest.</summary>
+		/// <param name="url">The API URL.</param>
+		/// <param name="json">The JSON string to be sent to the API.</param>
+		/// <param name="headers">The request headers.</param>
+		/// https://stackoverflow.com/a/8091963/10601212
+		/// https://stackoverflow.com/a/10027534/10601212
+		public static string POST(string url, string json, WebHeaderCollection headers)
+		{
+			HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+			request.ContentType = "application/json";
+			request.Method = "POST";
+
+			if (headers != null)
+			{
+				request.Headers = headers;
+			}
+
+			using (StreamWriter sw = new StreamWriter(request.GetRequestStream()))
+			{
+				sw.Write(json);
+			}
+
+			return ParseResponse(request.GetResponse());
+		}
+
+		/// <summary>Executes a POST Resquest.</summary>
+		/// <param name="url">The API URL.</param>
+		/// <param name="json">The JSON string to be sent to the API.</param>
+		/// https://stackoverflow.com/a/8091963/10601212
+		/// https://stackoverflow.com/a/10027534/10601212
+		public static string POST(string url, string json)
+		{
+			return POST(url, json, null);
+		}
+	}
+}
---
+++ ScriptsLib/Network/Requests/Requests.cs
@@ -0,0 +1,18 @@
+﻿using System.IO;
+using System.Net;
+using System.Text;
+
+namespace ScriptsLib.Network
+{
+	public static partial class Requests
+	{
+		public static string ParseResponse(WebResponse response)
+		{
+			using (Stream stream = response.GetResponseStream())
+			{
+				StreamReader reader = new StreamReader(stream, Encoding.UTF8);
+				return reader.ReadToEnd();
+			}
+		}
+	}
+}
