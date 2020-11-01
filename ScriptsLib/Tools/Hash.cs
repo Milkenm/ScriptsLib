a57f07c60b0e9e095abbc8b12b688810faf31b41
Milkenm <miguel.campos.johnny@gmail.com>
2020-11-01 01:10:28 +0000

    Update Hash.cs

--- ScriptsLib/Tools/Hash.cs
+++ ScriptsLib/Tools/Hash.cs
@@ -1,39 +1,24 @@
-﻿#region Usings
-using System;
-using System.Security.Cryptography;
+﻿using System.Security.Cryptography;
 using System.Text;
-using static ScriptsLib.Dev;
-#endregion Usings
-
-// # = #
-// https://stackoverflow.com/questions/3984138/hash-string-in-c-sharp
-// # = #
 
 namespace ScriptsLib
 {
 	public static partial class Tools
 	{
 		/// <summary>Converts the given string to SHA-256.</summary>
-		/// <param name="_String">The string to be converted.</param>
-		public static string Hash(string _String)
+		/// <param name="text">The string to be converted.</param>
+		/// https://stackoverflow.com/questions/3984138/hash-string-in-c-sharp
+		public static string Hash(string text)
 		{
-			try
-			{
-				HashAlgorithm _Hash = SHA256.Create();
-				StringBuilder _Builder = new StringBuilder();
+			HashAlgorithm ha = SHA256.Create();
+			StringBuilder sb = new StringBuilder();
 
-				foreach (byte _Byte in _Hash.ComputeHash(Encoding.UTF8.GetBytes(_String)))
-				{
-					_Builder.Append(_Byte.ToString("X2"));
-				}
-
-				return _Builder.ToString();
-			}
-			catch (Exception _Exception)
+			foreach (byte b in ha.ComputeHash(Encoding.UTF8.GetBytes(text)))
 			{
-				Msg(_Exception.Message, MsgType.Error, _Exception.Source);
-				return null;
+				sb.Append(b.ToString("X2"));
 			}
+
+			return sb.ToString();
 		}
 	}
 }
