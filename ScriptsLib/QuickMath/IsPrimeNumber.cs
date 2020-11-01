2b5e42aa2f6c99ffacf07503f8d4eab8e2081221
Milkenm <miguel.campos.johnny@gmail.com>
2020-11-01 00:53:48 +0000

    Update QuickMath (Math)

--- ScriptsLib/QuickMath/IsPrimeNumber.cs
+++ ScriptsLib/QuickMath/IsPrimeNumber.cs
@@ -3,17 +3,18 @@
 	public static partial class QuickMath
 	{
 		/// <summary>Returns <see langword="true"/> if the provided number is prime, else returns <see langword="false"/>.</summary>
-		/// <param name="_Number">The number to check.</param>
-		public static bool IsPrimeNumber(int _Number)
+		/// <param name="number">The number to check.</param>
+		public static bool IsPrimeNumber(int number)
 		{
-			for (int _Loop = 2; _Loop < _Number; _Loop++)
+			for (int i = 2; i < number; i++)
 			{
-				if (_Number % _Loop == 0)
+				if (number % i == 0)
 				{
 					return false;
 				}
 			}
-			return _Number > 1;
+
+			return number > 1;
 		}
 	}
 }
