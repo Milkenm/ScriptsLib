d0e1afb022a8b8cb87f5088d3d892607a8b4f48a
Milkenm <miguel.campos.johnny@gmail.com>
2020-11-01 22:43:28 +0000

    Update SendSmsViaEmail.cs

--- ScriptsLib/Network/Mobile/SendSmsViaEmail.cs
+++ ScriptsLib/Network/Mobile/SendSmsViaEmail.cs
@@ -5,30 +5,29 @@ namespace ScriptsLib.Network
 {
 	public static partial class Mobile
 	{
-		/// <summary>[WIP] It doesn't work lmao. (report this if u see)</summary>
-		/// <param name="_SenderEmail">The e-mail from where the SMS will be sent.</param>
-		/// <param name="_SenderEmailPassword">The password required to access the sender's e-mail.</param>
-		/// <param name="_ReceiverPhoneNumber">The phone number to where the SMS will be sent.</param>
-		/// <param name="_Subject">The title of the SMS.</param>
-		/// <param name="_Message">The message (or the SMS).</param>
-		/// <param name="_SmsCarrier">Idk (report if u see this).</param>
-		/// <param name="_SmtpHost">Idk (report if u see this).</param>
-		/// <param name="_SmtpPort">Idk (report if u see this).</param>
-		public static void SendSmsViaEmail(string _SenderEmail, string _SenderEmailPassword, string _ReceiverPhoneNumber, string _Subject, string _Message, string _SmsCarrier = "txt.att.net", string _SmtpHost = "smtp.gmail.com", int _SmtpPort = 587)
+		/// <param name="senderEmail">The e-mail from where the SMS will be sent.</param>
+		/// <param name="senderEmailPassword">The password required to access the sender's e-mail.</param>
+		/// <param name="receiverPhoneNumber">The phone number to where the SMS will be sent.</param>
+		/// <param name="subject">The title of the SMS.</param>
+		/// <param name="message">The message (or the SMS).</param>
+		public static void SendSmsViaEmail(string senderEmail, string senderEmailPassword, string receiverPhoneNumber, string subject, string message, string smsCarrier = "txt.att.net", string smtpHost = "smtp.gmail.com", int smtpPort = 587)
 		{
-			// Gateway.
-			string _Recipent = string.Concat(new object[] { _ReceiverPhoneNumber, '@', _SmsCarrier });
+			// Gateway
+			string recipent = string.Concat(new object[] { receiverPhoneNumber, '@', smsCarrier });
 
-			// Form the message.
-			MailMessage _Sms = new MailMessage(_SenderEmail, _Recipent, _Subject, _Message);
+			// Form the message
+			MailMessage sms = new MailMessage(senderEmail, recipent, subject, message);
 
-			// Create the client...
-			SmtpClient _SmsClient = new SmtpClient(_SmtpHost, _SmtpPort);
-			_SmsClient.UseDefaultCredentials = false;
-			_SmsClient.EnableSsl = true;
-			_SmsClient.Credentials = new NetworkCredential(_SenderEmail, _SenderEmailPassword);
-			// ...and then send it.
-			_SmsClient.Send(_Sms);
+			// Create the client
+			SmtpClient smsClient = new SmtpClient(smtpHost, smtpPort)
+			{
+				UseDefaultCredentials = false,
+				EnableSsl = true,
+				Credentials = new NetworkCredential(senderEmail, senderEmailPassword),
+			};
+
+			// Send the SMS
+			smsClient.Send(sms);
 		}
 	}
 }
