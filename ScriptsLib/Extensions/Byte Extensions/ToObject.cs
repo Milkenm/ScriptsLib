462bff7e20aee1a48011e432528d213ae23244ac
Milkenm <miguel.campos.johnny@gmail.com>
2020-11-02 02:21:10 +0000

    Update BinaryObjectConverter

---
+++ ScriptsLib/Extensions/Byte Extensions/ToObject.cs
@@ -0,0 +1,29 @@
+﻿using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace ScriptsLib.Extensions
+{
+	public static class ByteExtensions
+	{
+		/// <summary>Converts a byte array to an object.</summary>
+		/// <param name="byteArray">The byte array to convert to an object.</param>
+		public static object ToObject(byte[] byteArray)
+		{
+			if (byteArray.Length > 0)
+			{
+				using (MemoryStream ms = new MemoryStream())
+				{
+					ms.Write(byteArray, 0, byteArray.Length);
+					ms.Seek(0, SeekOrigin.Begin);
+
+					BinaryFormatter bf = new BinaryFormatter();
+					return bf.Deserialize(ms);
+				}
+			}
+			else
+			{
+				return null;
+			}
+		}
+	}
+}
