e34f034c2ee899bf9243ed884dc0b79ab75c1736
Milkenm <miguel.campos.johnny@gmail.com>
2020-11-02 02:48:32 +0000

    Update v1.0.0.0

--- ScriptsLib/Properties/Info.cs
+++ ScriptsLib/Properties/Info.cs
@@ -13,13 +13,13 @@ namespace ScriptsLib
 		public static readonly string Contributors;
 
 		/// <summary>Current ScriptsLib version.</summary>
-		public static readonly string Version = "6.3.1";
+		public static readonly string Version = "1.0.0.0";
 
 		/// <summary>First release date.</summary>
 		public static readonly string ReleaseDate = "30/04/2019 - 21:12";
 
 		/// <summary>Lattest release date.</summary>
-		public static readonly string UpdateDate = "19/02/2020 - 15:01";
+		public static readonly string UpdateDate = "02/11/2020 - 02:48";
 
 		// # ================================================================================================ #
 		/// <summary>AppData Path.</summary>
