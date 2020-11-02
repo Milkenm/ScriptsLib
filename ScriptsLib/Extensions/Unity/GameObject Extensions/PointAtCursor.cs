597ae0a84902f9339a7132604f3ac571f2905382
Milkenm <miguel.campos.johnny@gmail.com>
2020-11-02 01:39:17 +0000

    Update Unity

---
+++ ScriptsLib/Extensions/Unity/GameObject Extensions/PointAtCursor.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+using static ScriptsLib.Unity.Unity2d;
+
+namespace ScriptsLib.Extensions.Unity
+{
+	public static class GameObjectExtensions
+	{
+		public static void PointAtCursor(this GameObject obj)
+		{
+			obj.transform.rotation = Quaternion.Euler(0, 0, GetAngleRelativeToObject(obj.transform.position, Camera.main.ScreenToWorldPoint(Input.mousePosition)));
+		}
+	}
+}
