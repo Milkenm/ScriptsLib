5c7b0e651ee53696a3f93b0c23eeeec31f2cc12c
Milkenm <miguel.campos.johnny@gmail.com>
2020-11-02 02:15:00 +0000

    Update ShowException.cs

--- ScriptsLib/Tools/ShowException.cs
+++ ScriptsLib/Tools/ShowException.cs
@@ -1,32 +1,20 @@
-﻿#region Usings
-using System;
-using System.Threading.Tasks;
+﻿using System;
 using System.Windows.Forms;
-#endregion Usings
-
-// # = #
-// https://stackoverflow.com/questions/3328990/c-sharp-get-line-number-which-threw-exception
-// # = #
 
 namespace ScriptsLib
 {
 	public static partial class Tools
 	{
 		/// <summary>Formats exceptions into a pretty message.</summary>
-		/// <param name="_Exception">The exception itself.</param>
-		public static async Task ShowException(Exception _Exception, string _ExceptionTitle = null)
+		/// <param name="ex">The exception itself.</param>
+		/// https://stackoverflow.com/questions/3328990/c-sharp-get-line-number-which-threw-exception
+		public static void ShowException(Exception ex, string messageTitle = null)
 		{
-			await Task.Factory.StartNew(() =>
-			{
-				if (!string.IsNullOrEmpty(_ExceptionTitle))
-				{
-					MessageBox.Show($"{_Exception.Message}\n\n\n\nLine: {new System.Diagnostics.StackTrace(_Exception, true).GetFrame(0).GetFileLineNumber()}", _ExceptionTitle, MessageBoxButtons.OK, MessageBoxIcon.Error);
-				}
-				else
-				{
-					MessageBox.Show($"{_Exception.Message}\n\n\n\nLine: {new System.Diagnostics.StackTrace(_Exception, true).GetFrame(0).GetFileLineNumber()}", $"Error - {_Exception.Source}", MessageBoxButtons.OK, MessageBoxIcon.Error);
-				}
-			});
+			string title = !string.IsNullOrEmpty(messageTitle)
+				? messageTitle
+				: $"Error - {ex.Source}";
+
+			MessageBox.Show($"{ex.Message}\n\n\n\nLine: {new System.Diagnostics.StackTrace(ex, true).GetFrame(0).GetFileLineNumber()}", title, MessageBoxButtons.OK, MessageBoxIcon.Error);
 		}
 	}
 }
