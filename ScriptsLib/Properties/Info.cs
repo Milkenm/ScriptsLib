024b524232cd35073890e27e49af53630f1bee56
Milkenm <miguel.campos.johnny@gmail.com>
2020-11-06 14:28:17 +0000

    Forgot the version update commit

--- ScriptsLib/Properties/Info.cs
+++ ScriptsLib/Properties/Info.cs
@@ -13,7 +13,7 @@ namespace ScriptsLib
 		public static readonly string Contributors;
 
 		/// <summary>Current ScriptsLib version.</summary>
-		public static readonly string Version = "1.0.0.0";
+		public static readonly string Version = "7.0.0";
 
 		/// <summary>First release date.</summary>
 		public static readonly string ReleaseDate = "30/04/2019 - 21:12";
