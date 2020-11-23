da4ed02f9e3163f56c6cb925481d13d15b09db5d
Milkenm <miguel.campos.johnny@gmail.com>
2020-11-23 01:10:13 +0000

    Add IsDebugMode

---
+++ ScriptsLib/Unsorted/IsDebugMode.cs
@@ -0,0 +1,21 @@
+﻿using System.Diagnostics;
+
+namespace ScriptsLib.Unsorted
+{
+	public static class Unsorted
+	{
+		private static bool Debugging;
+
+		public static bool IsDebugMode()
+		{
+			CheckDebugMode();
+			return Debugging;
+		}
+
+		[Conditional("DEBUG")]
+		private static void CheckDebugMode()
+		{
+			Debugging = true;
+		}
+	}
+}
