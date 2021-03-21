40a3c92ba8eb89da87d13b13d1c22c847eb33193
Milkenm <miguel.campos.johnny@gmail.com>
2021-03-21 03:29:48 +0000

    Add "ForceDirectoryDelete"

---
+++ ScriptsLib/Unsorted/ForceDirectoryDelete.cs
@@ -0,0 +1,26 @@
+﻿using System.IO;
+
+namespace ScriptsLib.Unsorted
+{
+	public partial class Unsorted
+	{
+		public static void ForceDirectoryDelete(string directoryPath) // https://stackoverflow.com/a/329502
+		{
+			string[] files = Directory.GetFiles(directoryPath);
+			string[] dirs = Directory.GetDirectories(directoryPath);
+
+			foreach (string file in files)
+			{
+				File.SetAttributes(file, FileAttributes.Normal);
+				File.Delete(file);
+			}
+
+			foreach (string dir in dirs)
+			{
+				ForceDirectoryDelete(dir);
+			}
+
+			Directory.Delete(directoryPath, false);
+		}
+	}
+}
