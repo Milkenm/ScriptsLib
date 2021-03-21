4541037edc21e8faac4b174c54996b324107bfb7
Milkenm <miguel.campos.johnny@gmail.com>
2021-03-21 04:14:30 +0000

    Add FolderManager

---
+++ ScriptsLib/Extensions/ScriptsLib/FolderManager Extesions/CreateFolders.cs
@@ -0,0 +1,26 @@
+﻿using ScriptsLib.FolderManager;
+
+using System.Collections.Generic;
+
+namespace ScriptsLib.Extensions.ScriptsLib
+{
+	public static class FolderManagerExtesions
+	{
+		/// <summary>Creates all the folders in the array unless they already exist.</summary>
+		/// <param name="folders">The array of folders to create.</param>
+		public static void CreateFolders(this Folder[] folders)
+		{
+			foreach (Folder folder in folders)
+			{
+				folder.CreateFolder();
+			}
+		}
+
+		/// <summary>Creates all the folders in the list unless they already exist.</summary>
+		/// <param name="folders">The list of folders to create.</param>
+		public static void CreateFolders(this List<Folder> folders)
+		{
+			CreateFolders(folders.ToArray());
+		}
+	}
+}
