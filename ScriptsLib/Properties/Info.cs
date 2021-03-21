7abce44ba106a5e44fa5cff34a74395a3f7848d5
Milkenm <miguel.campos.johnny@gmail.com>
2021-03-21 04:20:16 +0000

    Update version to 7.1.0 & rename ScriptsLib.Info.UpdateDate to ScriptsLib.Info.LatestUpdateDate

--- ScriptsLib/Properties/Info.cs
+++ ScriptsLib/Properties/Info.cs
@@ -13,13 +13,13 @@ namespace ScriptsLib
 		public static readonly string Contributors;
 
 		/// <summary>Current ScriptsLib version.</summary>
-		public static readonly string Version = "7.0.0";
+		public static readonly string Version = "7.1.0";
 
 		/// <summary>First release date.</summary>
 		public static readonly string ReleaseDate = "30/04/2019 - 21:12";
 
 		/// <summary>Lattest release date.</summary>
-		public static readonly string UpdateDate = "02/11/2020 - 02:48";
+		public static readonly string LatestUpdateDate = "21/03/2021 - 04:19";
 
 		// # ================================================================================================ #
 		/// <summary>AppData Path.</summary>
