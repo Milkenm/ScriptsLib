3e42b0f6e391f50bb0f33963550c7bb4d0330dd4
Milkenm <miguel.campos.johnny@gmail.com>
2022-03-15 15:25:53 +0000

    GitHub API

---
+++ ScriptsLibR/APIs/GitHub API/API/Get Latest Release.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ScriptsLibR.APIs
+{
+    public partial class GitHubAPI
+    {
+		public Release GetLatestRelease(string user, string repo)
+		{
+			HttpWebRequest request = GetHttpWebRequest($"{APIOrigin}repos/{user}/{repo}/releases/latest");
+			request.UserAgent = "request";
+			return GET<Release>(request);
+		}
+	}
+}
---
+++ ScriptsLibR/APIs/GitHub API/GitHub API.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ScriptsLibR.APIs
+{
+    public partial class GitHubAPI
+    {
+        private readonly string APIOrigin = "https://api.github.com/";
+    }
+}
