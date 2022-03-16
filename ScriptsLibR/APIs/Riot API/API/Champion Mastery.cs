e8f352233618a355eb555a8bec6852068892057e
Milkenm <miguel.campos.johnny@gmail.com>
2022-03-16 17:16:17 +0000

    Riot API (unfinished)

---
+++ ScriptsLibR/APIs/Riot API/API/Champion Mastery.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ScriptsLibR.APIs.RiotAPI
+{
+    public partial class RiotAPI
+    {
+		public class ChampionMastery // v4
+		{
+			/// <summary>Get all champion mastery entries sorted by number of champion points descending.</summary>
+			/// <param name="encryptedSummonerId">Summoner ID associated with the player.</param>
+			public List<ChampionMasteryDTO> GetChampionMasteryList(Region region, string encryptedSummonerId)
+			{
+				string request = GET(GetServerString(region) + $"/lol/champion-mastery/v4/champion-masteries/by-summoner/{encryptedSummonerId}" + GetAPIParameter());
+				return JsonConvert.DeserializeObject<List<ChampionMasteryDTO>>(request);
+			}
+
+			/// <summary>Get a champion mastery by player ID and champion ID.</summary>
+			/// <param name="encryptedSummonerId">Summoner ID associated with the player.</param>
+			/// <param name="championId">Champion ID to retrieve Champion Mastery for.</param>
+			public ChampionMasteryDTO GetMasteryByChampion(Region region, string encryptedSummonerId, long championId)
+			{
+				string request = GET(GetServerString(region) + $"/lol/champion-mastery/v4/champion-masteries/by-summoner/{encryptedSummonerId}/by-champion/{championId}" + GetAPIParameter());
+				return JsonConvert.DeserializeObject<ChampionMasteryDTO>(request);
+			}
+
+			/// <summary>Get a player's total champion mastery score, which is the sum of individual champion mastery levels.</summary>
+			/// <param name="encryptedSummonerId">Summoner ID associated with the player.</param>
+			public int GetTotalMasteryScore(Region region, string encryptedSummonerId)
+			{
+				return Convert.ToInt32(GET(GetServerString(region) + $"/lol/champion-mastery/v4/scores/by-summoner/{encryptedSummonerId}" + GetAPIParameter()));
+			}
+		}
+	}
+}
---
+++ ScriptsLibR/APIs/Riot API/Riot API.cs
@@ -0,0 +1,30 @@
+﻿namespace ScriptsLibR.APIs.RiotAPI
+{
+    public partial class RiotAPI
+    {
+        public RiotAPI(string apiKey)
+        {
+            this.APIKey = apiKey;
+        }
+
+        public const string RiotAPIOrigin = "https://{0}.api.riotgames.com";
+
+        public string APIKey { get; private set; }
+
+        private string GetAPIParameter()
+        {
+            return $"?api_key={this.APIKey}";
+        }
+
+        private string GetServerString(Region region)
+        {
+            return string.Format(RiotAPIOrigin, region);
+        }
+
+        /// <summary>Url is something like: $"/lol/champion-mastery/v4/champion-masteries/by-summoner/{encryptedSummonerId}"</summary>
+        private string MakeRequest(Region region, string url)
+        {
+            return GET(this.GetServerString(region) + url + this.GetAPIParameter());
+        }
+    }
+}
