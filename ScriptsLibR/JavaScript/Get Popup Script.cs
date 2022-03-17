3ebd1624753aa7e849bb45f8268003a3d43ad507
Milkenm <miguel.campos.johnny@gmail.com>
2022-03-17 02:41:13 +0000

    Create Get Popup Script.cs

---
+++ ScriptsLibR/JavaScript/Get Popup Script.cs
@@ -0,0 +1,10 @@
+﻿namespace ScriptsLibR.JavaScript
+{
+    public partial class JavaScript
+    {
+        public string GetPopupScript(string message)
+        {
+            return $"<script language=javascript>alert('{message}')</script>";
+        }
+    }
+}
