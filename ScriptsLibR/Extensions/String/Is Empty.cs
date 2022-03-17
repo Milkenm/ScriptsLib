604604f964df3c820d8a5228a0d64f45c4d3bbef
Milkenm <miguel.campos.johnny@gmail.com>
2022-03-17 11:08:47 +0000

    String.IsEmpty() extension

---
+++ ScriptsLibR/Extensions/String/Is Empty.cs
@@ -0,0 +1,35 @@
+﻿namespace ScriptsLibR.Extensions
+{
+	public static class StringExtensions
+	{
+		public static bool IsEmpty(this string str, bool ignoreSpaces)
+		{
+			bool isEmpty = string.IsNullOrEmpty(str);
+			if (ignoreSpaces)
+			{
+				isEmpty = string.IsNullOrWhiteSpace(str);
+			}
+			return isEmpty;
+		}
+
+		public static bool IsEmpty(this string str)
+		{
+			return IsEmpty(str, true);
+		}
+
+		public static bool IsEmpty(this string[] strs, bool ignoreSpaces)
+		{
+			bool isEmpty = true;
+			foreach (string str in strs)
+			{
+				isEmpty = isEmpty && str.IsEmpty(ignoreSpaces);
+			}
+			return isEmpty;
+		}
+
+		public static bool IsEmpty(this string[] strs)
+		{
+			return IsEmpty(strs, true);
+		}
+	}
+}
