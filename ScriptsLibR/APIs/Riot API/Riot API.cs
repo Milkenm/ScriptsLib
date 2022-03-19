8ac126dd002e72ce235d95e95bc36cbe1cf0c832
Milkenm <miguel.campos.johnny@gmail.com>
2022-03-19 03:46:31 +0000

    Update GetInstance()

--- ScriptsLibR/APIs/Riot API/Riot API.cs
+++ ScriptsLibR/APIs/Riot API/Riot API.cs
@@ -1,52 +1,58 @@
-﻿using Newtonsoft.Json;
-
+﻿
 using ScriptsLibR.Exceptions;
 
 namespace ScriptsLibR.APIs.RiotAPI
 {
-    public partial class RiotAPI
-    {
-        private static RiotAPI Instance;
-        public RiotAPI GetInstance()
-        {
-            if (Instance == null)
-            {
-                throw new NotInitializedException(this);
-            }
-            return Instance;
-        }
+	public partial class RiotAPI
+	{
+		private static RiotAPI Instance;
+		public static RiotAPI GetInstance()
+		{
+			if (Instance == null)
+			{
+				throw new NotInitializedException(Instance);
+			}
+			return Instance;
+		}
 
-        public RiotAPI(string apiKey)
-        {
-            this.APIKey = apiKey;
-            Instance = this;
-        }
+		public RiotAPI(string apiKey)
+		{
+			this.APIKey = apiKey;
+			Instance = this;
+		}
 
-        public const string RiotAPIOrigin = "https://{0}.api.riotgames.com";
+		public const string RiotAPIOrigin = "https://{0}.api.riotgames.com";
 
-        public string APIKey { get; private set; }
+		public string APIKey { get; private set; }
 
-        private string GetAPIParameter()
-        {
-            return $"?api_key={this.APIKey}";
-        }
+		private string GetAPIParameter()
+		{
+			return $"?api_key={this.APIKey}";
+		}
 
-        private string GetServerString(Region region)
-        {
-            return string.Format(RiotAPIOrigin, region);
-        }
+		private string GetServerString(Region region)
+		{
+			return string.Format(RiotAPIOrigin, region);
+		}
 
-        /// <summary>Url is something like: $"/lol/champion-mastery/v4/champion-masteries/by-summoner/{encryptedSummonerId}"</summary>
-        private T MakeGETRequest<T>(Region region, string url)
-        {
+		/// <summary>Url is something like: $"/lol/champion-mastery/v4/champion-masteries/by-summoner/{encryptedSummonerId}"</summary>
+		private T MakeGETRequest<T>(Region region, string url)
+		{
+			/*
             string response = GET(this.GetServerString(region) + url + this.GetAPIParameter());
             return JsonConvert.DeserializeObject<T>(response);
-        }
+            */
+			// TODO
+			return this.MakeGETRequest<T>(region, url);
+		}
 
-        private T MakePOSTRequest<T>(Region region, string url, string? json)
-        {
+		private T MakePOSTRequest<T>(Region region, string url, string? json)
+		{
+			/*
             string request = POST(this.GetServerString(region) + "/lol/tournament-stub/v4/codes" + this.GetAPIParameter(), json);
             return JsonConvert.DeserializeObject<T>(request);
-        }
-    }
+            */
+			return this.MakePOSTRequest<T>(region, url, json);
+		}
+	}
 }
