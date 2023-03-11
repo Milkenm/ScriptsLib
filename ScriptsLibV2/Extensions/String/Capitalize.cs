8994ab3bcc03db1fee9c0662f5e5672dbe25f1c0
Milkenm <miguel.campos.johnny@gmail.com>
2023-03-11 16:06:38 +0000

    Create Capitalize.cs

---
+++ ScriptsLibV2/Extensions/String/Capitalize.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+
+namespace ScriptsLibV2.Extensions
+{
+	public static partial class StringExtensions
+	{
+		public static string Capitalize(this string text)
+		{
+			string[] split = text.Split(' ');
+			List<string> result = new List<string>();
+			foreach (string s in split)
+			{
+				string firstLetter = s[0].ToString().ToUpper();
+				string rest = s.Substring(1, s.Length - 1).ToLower();
+				result.Add(string.Concat(firstLetter, rest));
+			}
+			return string.Join(" ", result);
+		}
+	}
+}
