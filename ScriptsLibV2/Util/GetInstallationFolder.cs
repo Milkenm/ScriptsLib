3409820bd6cd87d12e38f9b0575c24e7b0816e58
Milkenm <miguel.campos.johnny@gmail.com>
2023-03-15 14:46:50 +0000

    Create GetInstallationFolder.cs

---
+++ ScriptsLibV2/Util/GetInstallationFolder.cs
@@ -0,0 +1,13 @@
+﻿using System.IO;
+using System.Reflection;
+
+namespace ScriptsLibV2.Util
+{
+	public static partial class Utils
+	{
+		public static string GetInstallationFolder()
+		{
+			return Path.GetDirectoryName(Assembly.GetEntryAssembly().Location);
+		}
+	}
+}
