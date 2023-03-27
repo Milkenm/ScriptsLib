d2e8f7ad554099fa00ee15379dd6070ae393aea0
Milkenm <miguel.campos.johnny@gmail.com>
2023-03-27 11:41:01 +0100

    Make GeneratePassword a single function with optional parameters

--- ScriptsLibV2/Util/GeneratePassword.cs
+++ ScriptsLibV2/Util/GeneratePassword.cs
@@ -4,35 +4,17 @@ namespace ScriptsLibV2.Util
 {
 	public static partial class Utils
 	{
-		/// <summary>Generates a 16 characters long password (or call it string with random chars) using a-Z 0-9.</summary>
-		public static string GeneratePassword()
-		{
-			return GeneratePassword(16);
-		}
-
-		/// <summary>Generates a password (or call it string with random chars).</summary>
-		/// <param name="size">The amount of characters.</param>
-		public static string GeneratePassword(int size)
-		{
-			return GeneratePassword(size, "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890");
-		}
-
-		/// <summary>Generates a password (or call it string with random chars).</summary>
-		/// <param name="allowedChars">The characters to be used to make the password.</param>
-		public static string GeneratePassword(string allowedChars)
-		{
-			return GeneratePassword(16, allowedChars);
-		}
+		public const string DefaultPasswordCharacterSet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
 
 		/// <summary>Generates a password (or call it string with random chars).</summary>
 		/// <param name="size">The amount of characters.</param>
 		/// <param name="allowedChars">The characters to be used to make the password.</param>
-		public static string GeneratePassword(int size, string allowedChars)
+		public static string GeneratePassword(long size = 16, string allowedChars = DefaultPasswordCharacterSet)
 		{
 			string password = null;
 			Random rand = new Random();
 
-			for (int i = 0; i < size; i++)
+			for (long i = 0; i < size; i++)
 			{
 				password = $"{password}{allowedChars[rand.Next(allowedChars.Length - 1)]}";
 			}
