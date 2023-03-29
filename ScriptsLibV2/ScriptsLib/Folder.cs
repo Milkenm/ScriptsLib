f042fd88bd869b768cf6f623d8a339f13024559a
Milkenm <miguel.campos.johnny@gmail.com>
2023-03-29 18:39:35 +0100

    Stuff

--- ScriptsLibV2/ScriptsLib/Folder.cs
+++ ScriptsLibV2/ScriptsLib/Folder.cs
@@ -1,18 +1,25 @@
 ﻿using System.IO;
 
+using ScriptsLibV2.Util;
+
 namespace ScriptsLibV2
 {
 	public class Folder
 	{
-		/// <summary>Easily create and delete directories.</summary>
+		/// <summary>
+		/// Easily create and delete directories.
+		/// </summary>
+		public Folder() { }
+
+		/// <inheritdoc cref="Folder()"/>
 		/// <param name="path">The path to the folder.</param>
 		public Folder(string path)
 		{
 			Path = path;
 		}
 
-		/// <summary>Easily create and delete directories.</summary>
-		/// <param name="path">The path to the folder.</param>
+		/// <inheritdoc cref="Folder(string)"/>
+		/// <param name="path"/>
 		/// <param name="create"><see langword="true"/> to create the folder if it doesn't already exist.</param>
 		public Folder(string path, bool create)
 		{
@@ -23,22 +30,30 @@ namespace ScriptsLibV2
 			}
 		}
 
-		/// <summary>Stores the <see cref="string"/> path to the folder.</summary>
+		/// <summary>
+		/// Stores the <see cref="string"/> path to the folder.
+		/// </summary>
 		/// <returns>Returns the <see cref="string"/> path to the folder.</returns>
-		public string Path { get; }
+		public string Path { get; set; }
 
-		/// <summary>Override to return the folder name stored in the "Path" variable.</summary>
+		/// <summary>
+		/// Override to return the folder name stored in the "Path" variable.
+		/// </summary>
 		/// <returns>Returns a <see cref="string"/> the path to the folder.</returns>
 		public override string ToString()
 		{
 			return Path;
 		}
 
-		/// <summary>Returns a <see cref="bool"/> based on if the folder exists or not.</summary>
+		/// <summary>
+		/// Returns a <see cref="bool"/> based on if the folder exists or not.
+		/// </summary>
 		/// <returns>Returns <see langword="true"/> if the folder exists, else returns <see langword="false"/>.</returns>
 		public bool Exists => Directory.Exists(Path);
 
-		/// <summary>Creates the folder unless it already exists.</summary>
+		/// <summary>
+		/// Creates the folder unless it already exists.
+		/// </summary>
 		public void CreateFolder()
 		{
 			if (!Exists)
@@ -47,10 +62,21 @@ namespace ScriptsLibV2
 			}
 		}
 
-		/// <summary>Deletes the folder and all the files and other folders inside.</summary>
+		/// <summary>
+		/// Deletes the folder and all the files and other folders inside.
+		/// </summary>
 		public void DeleteFolder()
 		{
-			Util.Utils.DeleteDirectory(Path);
+			Utils.DeleteDirectory(Path);
+		}
+
+		/// <summary>
+		/// Gets the <see cref="DirectoryInfo"/> from the current path.
+		/// </summary>
+		/// <returns>A <see langword="new"/> <see cref="Directory"/> instance based on the current path.</returns>
+		public DirectoryInfo GetDirectory()
+		{
+			return new DirectoryInfo(Path);
 		}
 	}
 }
---
+++ ScriptsLibV2/Util/GetAssemblyByName.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+namespace ScriptsLibV2.Util
+{
+	public static partial class Utils
+	{
+		public static Assembly? GetAssemblyByName(string assemblyName)
+		{
+			IEnumerable<Assembly> matchingAssemblies = AppDomain.CurrentDomain.GetAssemblies().Where(assembly => assembly.GetName().Name == assemblyName);
+
+			if (matchingAssemblies.Any())
+			{
+				return matchingAssemblies.ElementAt(0);
+			}
+			return null;
+		}
+	}
+}
