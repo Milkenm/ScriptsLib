26c531196b8162a3dbb2d8e7cf64f32347e937de
Milkenm <miguel.campos.johnny@gmail.com>
2023-05-10 11:03:14 +0100

    Use #if DEBUG instead of the Conditional thing

--- ScriptsLibV2/Util/IsDebugEnabled.cs
+++ ScriptsLibV2/Util/IsDebugEnabled.cs
@@ -4,22 +4,17 @@ namespace ScriptsLibV2.Util
 {
 	public static partial class Utils
 	{
-		private static bool Debugging = false;
-
 		/// <summary>
 		/// Uses [<see cref="ConditionalAttribute"/>("DEBUG")] to check if debugging is enabled.
 		/// </summary>
 		/// <returns><see langword="true"/> if debugging is enabled, else <see langword="false"/></returns>
 		public static bool IsDebugEnabled()
 		{
-			CheckDebugMode();
-			return Debugging;
-		}
-
-		[Conditional("DEBUG")]
-		private static void CheckDebugMode()
-		{
-			Debugging = true;
+#if DEBUG
+			return true;
+#else
+			return false;
+#endif
 		}
 	}
 }
